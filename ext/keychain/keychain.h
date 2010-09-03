789b36d6e17b9b6421863bc364ef7e96edb3f05e
OZAWA Sakuro <sakuro@2238club.org>
2010-09-03 14:42:23 +0900

    Tag each string attribute with encoding[1.9].
    [Keychain Services Reference](http://developer.apple.com/mac/library/documentation/Security/Reference/keychainservices/Reference/reference.html)

--- ext/keychain/keychain.h
+++ ext/keychain/keychain.h
@@ -14,3 +14,49 @@ void Init_keychain_constants();
 struct KeychainItem {
 	SecKeychainItemRef *itemRef;
 };
+
+#ifdef HAVE_RUBY_ENCODING_H
+#include <ruby/encoding.h>
+
+#define KEYCHAIN_ASCII_STR_NEW(str, len)  rb_usascii_str_new((str), (len))
+#define KEYCHAIN_UTF8_STR_NEW(str, len)   rb_enc_str_new((str), (len), rb_utf8_encoding())
+#define KEYCHAIN_BINARY_STR_NEW(str, len) rb_enc_str_new((str), (len), rb_ascii8bit_encoding())
+
+#define KEYCHAIN_ASCII_STR_NEW2(str)  rb_usascii_str_new2((str))
+#define KEYCHAIN_UTF8_STR_NEW2(str)   rb_enc_str_new((str), strlen((str)), rb_utf8_encoding())
+#define KEYCHAIN_BINARY_STR_NEW2(str) rb_enc_str_new((str), strlen((str)), rb_ascii8bit_encoding())
+
+#define KEYCHAIN_SET_ASCII_ENCODING(str) \
+	do { \
+		if ((str) != Qnil) { \
+			ENCODING_SET((str), rb_usascii_encindex()); \
+		} \
+	} while (0)
+#define KEYCHAIN_SET_UTF8_ENCODING(str) \
+	do { \
+		if ((str) != Qnil) { \
+			ENCODING_SET((str), rb_utf8_encindex()); \
+		} \
+	} while (0)
+#define KEYCHAIN_SET_BINARY_ENCODING(str) \
+do { \
+	if ((str) != Qnil) { \
+		ENCODING_SET((str), rb_ascii8bit_encindex()); \
+	} \
+} while (0)
+
+#else
+
+#define KEYCHAIN_ASCII_STR_NEW(str, len)  rb_str_new((str), (len))
+#define KEYCHAIN_UTF8_STR_NEW(str, len)   rb_str_new((str), (len))
+#define KEYCHAIN_BINARY_STR_NEW(str, len) rb_str_new((str), (len))
+
+#define KEYCHAIN_ASCII_STR_NEW2(str)  rb_str_new2((str))
+#define KEYCHAIN_UTF8_STR_NEW2(str)   rb_str_new((str), strlen((s)))
+#define KEYCHAIN_BINARY_STR_NEW2(str) rb_str_new((str), strlen((s)))
+
+#define KEYCHAIN_SET_ASCII_ENCODING(str) /* empty */
+#define KEYCHAIN_SET_UTF8_ENCODING(str) /* empty */
+#define KEYCHAIN_SET_BINARY_ENCODING(str) /* empty */
+
+#endif
